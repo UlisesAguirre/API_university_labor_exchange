d41b6726581cc4e5c86692801f805a8244665983
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-09-24 17:50:10 -0300

    change: models folder for entities folder

---
+++ API_university_labor_exchange/Entities/JobPositionsCareer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_university_labor_exchange.Entities;
+
+public partial class JobPositionsCareer
+{
+    public int IdJobPositionsCareers { get; set; }
+
+    public int IdJobPosition { get; set; }
+
+    public int IdCareer { get; set; }
+
+    public virtual Career IdCareerNavigation { get; set; } = null!;
+
+    public virtual JobPosition IdJobPositionNavigation { get; set; } = null!;
+}
