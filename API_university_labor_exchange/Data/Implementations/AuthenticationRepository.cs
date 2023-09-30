3c7e7b07c3787a5e7631ef3d7270223da39efee6
Clara Razzetto <clararazzetto@gmail.com>
2023-09-30 19:54:47 -0300

    change: authentication and register controller to add repository inheritance

--- API_university_labor_exchange/Data/Implementations/AuthenticationRepository.cs
+++ API_university_labor_exchange/Data/Implementations/AuthenticationRepository.cs
@@ -1,16 +1,13 @@
 ﻿using API_university_labor_exchange.Data.Interfaces;
+using API_university_labor_exchange.DBContext;
 using API_university_labor_exchange.Entities;
 
 namespace API_university_labor_exchange.Data.Implementations
 {
-    public class AuthenticationRepository :IAuthenticationRepository
+    public class AuthenticationRepository : Repository, IAuthenticationRepository
     {
-        private UniversityLaborExchangeContext _context;
-
-        public AuthenticationRepository(UniversityLaborExchangeContext context)
-        {
-            _context = context;
-        }
+      
+        public AuthenticationRepository (UniversityLaborExchangeContext context) : base(context) { }
 
         public User? ValidateUser(string email, string password) {
             return _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
